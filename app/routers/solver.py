a9cfbd05dd0f331c9b21f669568b1873ddf1ea86
iamklaus <iamklaus01@gmail.com>
2023-05-04 22:12:43 +0100

    Feat : Problem formulation extracted from intern module no longer distant api

--- app/routers/solver.py
+++ app/routers/solver.py
@@ -1,31 +1,51 @@
+import json
 from fastapi import APIRouter, HTTPException, status
-import httpx
-from typing import List
 from app.solver.solver import solve
 from app.models.problemSchema import DataIn
+
+from app.xtractor.core import helper
+
 router = APIRouter()
 
 
-# For user sign up
-@router.post("/solver/all", status_code=status.HTTP_200_OK)
-async def register(data:DataIn):
-    try:
-        solutions = solve(data.variables, data.constraints, data.optimization)
+@router.post("/resolve", status_code=status.HTTP_200_OK)
+async def get_solutions(entry:DataIn):
+    response = get_problem_formulation(entry.problem)
+    if(response['status'] == "success"):
+        print(response)
+        data = response['data']
+        # try:
+        solutions, variables, ct = solve(data['variables'], data['constraints'], [])
         if type(solutions) == str:
-            raise HTTPException(status_code=status.HTTP_200_OK, detail="No solutions found")
+            return {"message" : "No solutions founded!"}
         elif type(solutions) == list:
             if len(solutions) == 0:
-                raise HTTPException(status_code=status.HTTP_200_OK, detail="No solutions found")
+                return {"message" : "No solutions founded!"}
             else:
-                return {"message" : "OK", 'solutions': solutions}
-        
+                model_data = {
+                    'v': variables,
+                    'ct': ct
+                }
+                return {'message' : "OK", 'solutions': solutions, 'model_data': model_data, 'opt':[]}
+        # except Exception as e:
+        #     print(f"An exception has occurred: {e}")
+        #     raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="The formulation of the problem does not follow the expected protocol!")
+    else:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=data['error'])
+
+def get_problem_formulation(problem_description:str):
+    try:
+        problem_formulation = helper.grail_extractor(problem_description)
+        return{
+            "status":"success",
+            "data":problem_formulation
+        }
     except Exception as e:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+        return{
+            "status":"failed",
+            "data":None,
+            "error":e
+        }
 
 
-async def get_problem_formulation():
-    async with httpx.AsyncClient() as client:
-        response = await client.get("http://your-node-api-url.com/api-endpoint")
-        response.raise_for_status()
-        return response.json()
 
